f6d191eb760b17c5776e4a0f0f72c5a7b3c087c0
Rusydy <rusydymuhiddin@gmail.com>
2023-09-12 23:52:30 +0700

    ADD: webView feature to load a webiste

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,46 +1,22 @@
 package com.example.webview
 
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.webview.ui.theme.WebviewTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WebviewTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WebviewTheme {
-        Greeting("Android")
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://developer.android.com/develop/ui/views/layout/webapps/webview")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(true)
     }
+
 }
