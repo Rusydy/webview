10c1552dc66030504e6d630e6d3c636d46852ebb
Rusydy <rusydymuhiddin@gmail.com>
2023-09-16 04:27:12 +0700

    UPDATE: change the layout into simple two image with one text

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.webview
 
-import android.content.Intent
-import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import com.bumptech.glide.Glide
+import android.os.Bundle
 
 class MainActivity : ComponentActivity() {
 
@@ -12,11 +12,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView = findViewById<WebView>(R.id.webview)
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://developer.android.com/develop/ui/views/layout/webapps/webview")
-        webView.settings.javaScriptEnabled = true
-        webView.settings.setSupportZoom(true)
-    }
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val textView = findViewById<TextView>(R.id.textView)
 
+        // Load Image from URL using Glide and resize ImageView
+        val imageUrl = "https://d1jnx9ba8s6j9r.cloudfront.net/blog/wp-content/uploads/2019/06/Kotlin.png"
+        Glide.with(this)
+            .load(imageUrl)
+            .into(imageView)
+
+        // Load Image from URL using Glide and resize ImageView
+        val imageUrl2 = "https://res.cloudinary.com/andresbaravalle/image/upload/v1542975073/logo-javascript_2x_cpugmu.png"
+        Glide.with(this)
+            .load(imageUrl2)
+            .into(imageView2)
+
+        textView.text = "Text you want to make copyable"
+    }
 }
