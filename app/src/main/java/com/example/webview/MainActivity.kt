bac23dc100cc43822594d9dab64371042207a77e
Rusydy <rusydymuhiddin@gmail.com>
2024-01-15 21:07:56 +0700

    Tidy up indentation and imports

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -3,22 +3,18 @@ package com.example.webview
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.activity.ComponentActivity
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -29,11 +25,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat.getSystemService
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -54,20 +51,13 @@ class MainActivity : ComponentActivity() {
 fun WebViewScreen() {
     var loading by remember { mutableStateOf(true) }
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
     ) {
-
-//        if (internetQuality == InternetQuality.GOOD) {
-            WebViewComponent(
-                url = "https://www.google.com",
-                onLoadingStateChanged = { loading = it }
-            )
-//        } else {
-//        }
+        WebViewComponent(url = "https://www.google.com", onLoadingStateChanged = { loading = it })
     }
 }
 
+@OptIn(DelicateCoroutinesApi::class)
 @RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun WebViewComponent(url: String, onLoadingStateChanged: (Boolean) -> Unit) {
@@ -75,41 +65,37 @@ fun WebViewComponent(url: String, onLoadingStateChanged: (Boolean) -> Unit) {
     var progress by remember { mutableStateOf(0) }
     var internetQuality by remember { mutableStateOf(InternetQuality.UNKNOWN) }
 
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                webViewClient = object : WebViewClient() {
-                    override fun onPageFinished(view: WebView?, url: String?) {
-                        super.onPageFinished(view, url)
-                        GlobalScope.launch {
-                            delay(1000)
-                            onLoadingStateChanged(false)
-                            loading = false
-                        }
+    AndroidView(factory = { context ->
+        WebView(context).apply {
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    GlobalScope.launch {
+                        delay(1000)
+                        onLoadingStateChanged(false)
+                        loading = false
                     }
                 }
-
-                webChromeClient = object : WebChromeClient() {
-                    override fun onProgressChanged(view: WebView?, newProgress: Int) {
-                        super.onProgressChanged(view, newProgress)
-                        progress = newProgress
-                        // You can use 'progress' to update your UI accordingly
-                    }
-                }
-
-                loadUrl(url)
             }
-        },
-        update = { webView ->
-            webView.loadUrl(url)
-        },
-        modifier = Modifier.fillMaxSize()
+
+            webChromeClient = object : WebChromeClient() {
+                override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                    super.onProgressChanged(view, newProgress)
+                    progress = newProgress
+                    // You can use 'progress' to update your UI accordingly
+                }
+            }
+
+            loadUrl(url)
+        }
+    }, update = { webView ->
+        webView.loadUrl(url)
+    }, modifier = Modifier.fillMaxSize()
     )
 
     if (loading) {
         val context = LocalContext.current
-        val connectivityManager =
-            getSystemService(context, ConnectivityManager::class.java)
+        val connectivityManager = getSystemService(context, ConnectivityManager::class.java)
         val network = connectivityManager?.activeNetwork
         val networkCapabilities = connectivityManager?.getNetworkCapabilities(network)
 
@@ -123,8 +109,7 @@ fun WebViewComponent(url: String, onLoadingStateChanged: (Boolean) -> Unit) {
 @Composable
 fun LoadingProgress(progress: Int, internetQuality: InternetQuality) {
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
     ) {
         Column(
             modifier = Modifier
@@ -134,8 +119,7 @@ fun LoadingProgress(progress: Int, internetQuality: InternetQuality) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             CircularProgressIndicator(
-                modifier = Modifier
-                    .size(50.dp),
+                modifier = Modifier.size(50.dp),
                 color = MaterialTheme.colorScheme.primary,
                 progress = (progress / 100f).coerceIn(0f, 1f)
             )
@@ -149,10 +133,7 @@ fun LoadingProgress(progress: Int, internetQuality: InternetQuality) {
 }
 
 enum class InternetQuality {
-    UNKNOWN,
-    POOR,
-    MODERATE,
-    GOOD
+    UNKNOWN, POOR, MODERATE, GOOD
 }
 
 @RequiresApi(Build.VERSION_CODES.Q)
@@ -165,6 +146,7 @@ fun getInternetQuality(networkCapabilities: NetworkCapabilities): InternetQualit
                 else -> InternetQuality.POOR
             }
         }
+
         networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
             when (networkCapabilities.signalStrength) {
                 in -50..0 -> InternetQuality.GOOD
@@ -172,6 +154,7 @@ fun getInternetQuality(networkCapabilities: NetworkCapabilities): InternetQualit
                 else -> InternetQuality.POOR
             }
         }
+
         else -> InternetQuality.UNKNOWN
     }
 }
