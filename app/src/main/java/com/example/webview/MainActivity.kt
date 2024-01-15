bba6e771486c126ca58d7d22b7b35aa025bf96b7
Rusydy <rusydymuhiddin@gmail.com>
2024-01-15 10:28:25 +0700

    Add Webview with loading animation

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,33 +1,120 @@
 package com.example.webview
 
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.ComponentActivity
-import com.bumptech.glide.Glide
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            WebViewScreen()
+        }
+    }
+}
 
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        val imageView2 = findViewById<ImageView>(R.id.imageView2)
-        val textView = findViewById<TextView>(R.id.textView)
+@Composable
+fun WebViewScreen() {
+    var loading by remember { mutableStateOf(true) }
 
-        // Load Image from URL using Glide and resize ImageView
-        val imageUrl = "https://d1jnx9ba8s6j9r.cloudfront.net/blog/wp-content/uploads/2019/06/Kotlin.png"
-        Glide.with(this)
-            .load(imageUrl)
-            .into(imageView)
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        WebViewComponent(
+            url = "https://www.google.com",
+            onLoadingStateChanged = { loading = it }
+        )
+    }
+}
 
-        // Load Image from URL using Glide and resize ImageView
-        val imageUrl2 = "https://res.cloudinary.com/andresbaravalle/image/upload/v1542975073/logo-javascript_2x_cpugmu.png"
-        Glide.with(this)
-            .load(imageUrl2)
-            .into(imageView2)
+@Composable
+fun WebViewComponent(url: String, onLoadingStateChanged: (Boolean) -> Unit) {
+    var loading by remember { mutableStateOf(true) }
+    var progress by remember { mutableStateOf(0) }
 
-        textView.text = "Text you want to make copyable"
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                webViewClient = object : WebViewClient() {
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                        GlobalScope.launch {
+                            delay(1000)
+                            onLoadingStateChanged(false)
+                            loading = false
+                        }
+                    }
+                }
+
+                webChromeClient = object : WebChromeClient() {
+                    override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                        super.onProgressChanged(view, newProgress)
+                        progress = newProgress
+                        // You can use 'progress' to update your UI accordingly
+                    }
+                }
+
+                loadUrl(url)
+            }
+        },
+        update = { webView ->
+            webView.loadUrl(url)
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+
+    if (loading) {
+        LoadingProgress(progress = progress)
+    }
+}
+
+@Composable
+fun LoadingProgress(progress: Int) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(50.dp),
+                color = MaterialTheme.colorScheme.primary,
+                progress = (progress / 100f).coerceIn(0f, 1f)
+            )
+            Text(text = "Loading... $progress%")
+        }
     }
 }
